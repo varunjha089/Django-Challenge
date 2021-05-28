a65b4399de368a1f44b8b7fea34d3de73177f8bf
Varun Kumar <varunjha089@gmail.com>
2021-05-28 06:16:26 +0000

    added photos app to settings.py file.

--- 14_Photo_Album_App/mysite/settings.py
+++ 14_Photo_Album_App/mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Created Apps
+    'photos.apps.PhotosConfig',
 ]
 
 MIDDLEWARE = [
